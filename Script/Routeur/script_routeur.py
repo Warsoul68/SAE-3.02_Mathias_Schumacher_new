cfcaf6f1941fd7e32105a16e07da200c796b4236
Mathias <guzmer68@gmail.com>
2025-12-18 16:45:08 +0100

    Modif sur le script du routeur

--- Script/Routeur/script_routeur.py
+++ Script/Routeur/script_routeur.py
@@ -2,40 +2,18 @@ import sys
 from Routeur import Routeur
 
 def main():
-    print("Réseaux oignons : routeur hybride")
-
-    # 1. Récupération du port local via l'argument
     if len(sys.argv) < 2:
-        print("\n[!] Erreur : Vous devez spécifier un port pour ce routeur.")
-        print("Exemple : python3 script_routeur.py 8081")
+        print("Usage: python3 script_routeur.py <PORT_LOCAL>")
         sys.exit(1)
 
-    try:
-        port_local = int(sys.argv[1])
-    except ValueError:
-        print("[!] Erreur : Le port doit être un nombre entier.")
-        sys.exit(1)
-
-    # Saisie des informations du Master
-    print("\n--- Configuration de la connexion Master ---")
-    master_ip = input("Entrez l'IP du serveur Master (ex: 10.128.200.15) : ")
+    port_local = sys.argv[1]
     
-    while True:
-        try:
-            master_port = int(input("Entrez le Port du serveur Master (ex: 8080) : "))
-            break
-        except ValueError:
-            print("Veuillez entrer un numéro de port valide.")
+    print(f"--- Démarrage du Routeur Hybride ({port_local}) ---")
+    master_ip = input("IP du Master : ")
+    master_port = int(input("Port du Master : "))
 
-    # 3. Initialisation et démarrage
-    print(f"\n[*] Tentative de démarrage du routeur sur le port {port_local}...")
-    mon_routeur = Routeur(port_local, master_ip, master_port)
-    
-    try:
-        mon_routeur.demarrer()
-    except KeyboardInterrupt:
-        print("\n[!] Arrêt manuel du routeur.")
-        sys.exit(0)
+    mon_routeur = Routeur(int(port_local), master_ip, master_port)
+    mon_routeur.demarrer()
 
 if __name__ == "__main__":
     main()
